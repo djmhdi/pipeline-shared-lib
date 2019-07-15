baed2be1e19e55cc011fe316766fc66f12029d9f
ggortais <gwendal.gortais@sogeti.com>
2019-07-15 23:13:03 +0200

    [TECH] rename package

--- src/com/capgemini/fs/jenkins/plugins/util/HtmlTemplates.groovy
+++ src/com/capgemini/fs/jenkins/plugins/util/HtmlTemplates.groovy
@@ -2,15 +2,15 @@
 package com.capgemini.fs.jenkins.plugins.util
 
 enum HtmlTemplates {
-	HELLO('fr/pe/notification/html/HelloWorld.html'),
-	CONSTRUCTION('fr/pe/notification/html/ResultatConstructionVersion.tmpl'),
-	BUILD_SNAPSHOT('fr/pe/notification/html/ResultatBuildSnapshot.tmpl'),
-	DEMANDE_LIVRAISON('fr/pe/notification/html/DemandeLivraison.tmpl'),
-	LIVRAISON('fr/pe/notification/html/ResultatLivraison.tmpl'),
-    PROMOTION('fr/pe/notification/html/ResultatPromotion.tmpl'),
-    DEPLOIEMENT('fr/pe/notification/html/ResultatDeploiement.tmpl'),
-	VERIFICATION_DEPLOIEMENT('fr/pe/notification/html/ResultatVerificationDeploiement.tmpl'),
-	VERIFICATION_QUALITE('fr/pe/notification/html/ResultatVerificationQualite.tmpl')
+	HELLO('com/capgemini/fs/notification/html/HelloWorld.html'),
+	CONSTRUCTION('com/capgemini/fs/notification/html/ResultatConstructionVersion.tmpl'),
+	BUILD_SNAPSHOT('com/capgemini/fs/notification/html/ResultatBuildSnapshot.tmpl'),
+	DEMANDE_LIVRAISON('com/capgemini/fs/notification/html/DemandeLivraison.tmpl'),
+	LIVRAISON('com/capgemini/fs/notification/html/ResultatLivraison.tmpl'),
+    PROMOTION('com/capgemini/fs/notification/html/ResultatPromotion.tmpl'),
+    DEPLOIEMENT('com/capgemini/fs/notification/html/ResultatDeploiement.tmpl'),
+	VERIFICATION_DEPLOIEMENT('com/capgemini/fs/notification/html/ResultatVerificationDeploiement.tmpl'),
+	VERIFICATION_QUALITE('com/capgemini/fs/notification/html/ResultatVerificationQualite.tmpl')
 
     HtmlTemplates(String templatePath) {
 		this.path = templatePath
